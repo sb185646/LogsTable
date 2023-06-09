d867094b6f81bf198de1ab26f9874a80effd3c4b
Sreeman <sb185646@ncr.com>
2023-06-09 13:40:19 +0530

    added new logs

--- app.js
+++ app.js
@@ -1,34 +1,68 @@
 const { exec } = require('child_process');
 const express = require('express');
-const render = require('./table');
+const { render, html } = require('./utils');
 const app = express();
 app.use(express.json());
 const port = 3005;
+const commands = {
 
-app.get('/logs', (req, res) => {
+}
+
+const getPodNameWithoutHash = (n) => {
+    const p = n.split('-');
+    return `${p[0]}-${p[1]}`
+}
+app.use((req, res, next) => {
+    console.log('Requesting URL: ', req.url)
+    next()
+})
+app.get('/', (_, res) => {
+
+
+    res.send(html(`<div style=display:flex;align-items:center;justify-content:center;height:95vh;>
+    <button onclick="myJavascriptFunction()" class=app-button>Get Logs</button>
+    </div>`))
+})
+app.get('/pods', (req, res) => {
     exec('kubectl get pods -n store', (error, stdout, stderr) => {
         if (error) {
             console.error(`exec error: ${error}`);
             res.send('<h1>OOPS! Failed to get logs</h1>')
             return;
         }
-        res.send(stdout.split('\n').reduce((a, s) => {
-            const podName = s.split(' ')[0];
-            a += '<div style=margin:20px;white-space:break-spaces;>' + (`<a href=/pod-logs/${podName}>${podName}</a>` + '\n') + '</div>';
+        res.send(html(stdout.split('\n').reduce((a, s) => {
+            const { 0: podName, 2: status } = s.split(' ').filter(c => c);
+            if (podName) {
+                const showButtons = podName.toUpperCase() !== 'NAME'
+                    ? `
+                <form style=display:initial;margin:0; method="GET" action=/scale-pod-down/${podName}>
+                <button>${'Scale Down'}</button> 
+                </form>` : '';
+                a += `
+                <div style=margin:20px;white-space:break-spaces;display:flex;justify-content:space-evenly;align-items:center;>
+                <a href=/pod-logs/${podName}>${podName}</a>
+                <span>${status}</span>
+                ${showButtons}
+                <br/></div>
+                `;
+            }
             return a;
-        }, ''))
+        }, '')))
     });
 })
-app.use((req, res, next) => {
-    // res.send('(() => { console.log("ksaljd") })()');
-    next()
-})
-app.get('/pod-logs/:id', (req, res) => {
 
-    exec(`kubectl logs -n store ${req.params.id}`, (error, stdout, stderr) => {
+app.get('/pod-logs/:id', (req, res) => {
+    const query = `kubectl logs -n store ${req.params.id}`
+    console.log({ id: req.params.id, query })
+    exec(query, (error, stdout, stderr) => {
+        let finalRes = '';
         if (error) {
-            res.send('<h1>OOPS! Failed to get logs</h1>')
-            return;
+            try {
+                finalRes += `<h3>${error.message}</h3>`
+            } catch (e) {
+                res.send('<h1>OOPS! Failed to get logs</h1>', e)
+                return;
+            }
         }
         const errored = [];
         const logs = stdout.split('\n').filter(l => !l.includes('[Nest]'))
@@ -47,11 +81,32 @@ app.get('/pod-logs/:id', (req, res) => {
                     return { time: 'failed to parse the logs', message: c }
                 }
             })
-        const finalRes = render(logs);
+        finalRes += render(logs);
         res.send(finalRes)
     });
 })
 
+app.get('/scale-pod-up/:id', (req, res) => {
+    console.log(getPodNameWithoutHash(req.params.id))
+    exec(`kubectl scale deploy ${getPodNameWithoutHash(req.params.id)} -n store --replicas=1`, (error, stdout, stderr) => {
+        if (error) {
+            res.send('<h1>OOPS! Failed to scale up</h1>')
+            return;
+        }
+        res.send(stdout)
+    })
+})
+app.get('/scale-pod-down/:id', (req, res) => {
+    console.log(getPodNameWithoutHash(req.params.id))
+    exec(`kubectl scale deploy ${getPodNameWithoutHash(req.params.id)} -n store --replicas=0`, (error, stdout, stderr) => {
+        if (error) {
+            res.send('<h1>OOPS! Failed to scale down</h1>')
+            return;
+        }
+        res.send(stdout)
+    })
+})
+
 app.listen(port, () => {
-    console.log('Server started at ' + port, 'Check logs at http://localhost:3005/logs')
+    console.log('Server started at ' + port, 'Check logs at http://localhost:3005/pods')
 });
