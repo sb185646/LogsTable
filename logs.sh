3094d450867d91538bfb9457de15562392e532a7
Sreeman <sb185646@ncr.com>
2023-06-20 12:37:54 +0530

    Bash Script

---
+++ logs.sh
@@ -0,0 +1,7 @@
+echo "Changing the directory"
+cd "C:\Sreeman\LogsTable"
+echo "Starting npm..."
+npm start
+echo "Opening a terminal"
+echo "Server started browse at http://localhost:3005/logs"
+gnome-terminal
